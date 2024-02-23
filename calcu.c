8b160bcd6dbfc3bde16e6ae82713d9928e59f258
alaukikprabhakar8306@gmail.com <alaukikprabhakar8306@gmail.com>
2024-02-23 11:47:24 +0530

    this is the first commit

--- calcu.c
+++ calcu.c
@@ -1,6 +1,23 @@
-#include<stdioh>
+#include<stdio.h>
 int main(){
 	char c;
+	int a=20;
+	int b=10;
 	printf("give the operator");
 	scanf("%c",&c);
-}
+	switch (c){
+		case '+' :
+			printf("%d",a+b);
+			break;
+		case '-' :
+			printf("%d",a-b);
+			break;
+		case '*' :
+			printf("%d",a*b);
+			break;
+		case '/' :
+			printf("%d",a/b);
+			break;
+		default :
+			printf("invalid");
+	}}
